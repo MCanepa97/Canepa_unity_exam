3c20a961e1cce6701515ae00a1ef83f294803add
Mattia Canepa <icts22-24.466@A4-05.local>
2023-06-28 14:42:43 +0200

    iniziato lo script per farspostare gli oggetti quando colpiti, aggiunti sprite per il cheddar

--- Esame_unity_2D/Assets/Scripts/player/PlayerMovement.cs
+++ Esame_unity_2D/Assets/Scripts/player/PlayerMovement.cs
@@ -6,12 +6,95 @@ using System.Threading.Tasks;
 
 public class PlayerMovement : MonoBehaviour
 {
+    enum Facing
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    string barileToccato="no";
+    private Facing isFacing= Facing.Down;
+    private bool punching=false;
     [SerializeField] private float speed = 4f;
     private Rigidbody2D _rigidBody; // creates a variable that will store a rigidBody
     private Vector2 _movementInput; // creates a variable to store the player input, to be used in all methods
     private Vector2 _smoothedMovementInput; // a secondary movement vector to smooth out movement when input ends
     private Vector2 _velocityTracker; //a vector to keep track of the velocity of the change
     public Animator animator; //creazione di un animator
+
+    private async void OnCollisionEnter2D(Collision2D other) 
+    {
+        Vector2 punchingForce= new Vector2(0,10);
+        switch (isFacing)
+        {
+        case Facing.Down:
+        punchingForce= new Vector2(0,10);
+        break;
+        case Facing.Up:
+        punchingForce= new Vector2(0,-10);
+        break;
+        case Facing.Left:
+        punchingForce=new Vector2(-10,0);
+        break;
+        case Facing.Right:
+        punchingForce=new Vector2(10,0);
+        break;
+        }
+        if(other.collider.tag!="")
+        {
+        barileToccato="si";
+        Debug.Log("barileToccato="+barileToccato+punching);
+
+        await Task.Delay(500);
+        barileToccato="no";
+        }
+        
+         
+        if(other.collider.tag=="Barrell"&&punching)
+        {
+            Rigidbody2D barrellBody= other.collider.GetComponent<Rigidbody2D>();
+            barrellBody.constraints= RigidbodyConstraints2D.FreezeRotation;
+            barrellBody.AddForce(punchingForce, ForceMode2D.Impulse);
+            punching=false;
+        }    
+    }
+    
+    private async void OnCollisionStay2D(Collision2D other)
+    {
+        Vector2 punchingForce= new Vector2(0,10);
+        switch (isFacing)
+        {
+        case Facing.Down:
+        punchingForce= new Vector2(0,5);
+        break;
+        case Facing.Up:
+        punchingForce= new Vector2(0,-5);
+        break;
+        case Facing.Left:
+        punchingForce=new Vector2(-5,0);
+        break;
+        case Facing.Right:
+        punchingForce=new Vector2(5,0);
+        break;
+        }
+        if(other.collider.tag!="")
+        {
+        barileToccato="si";
+        Debug.Log("barileToccato="+barileToccato+punching);
+        await Task.Delay(500);
+        barileToccato="no";
+        }
+        
+         
+        if(other.collider.tag=="Barrell"&&punching)
+        {
+            Rigidbody2D barrellBody= other.collider.GetComponent<Rigidbody2D>();
+            barrellBody.constraints= RigidbodyConstraints2D.FreezeRotation;
+            barrellBody.AddForce(punchingForce, ForceMode2D.Impulse);
+            punching=false;
+        }  
+    }
     private void Awake() //calling this method when the scene is 1st initialized
     {
         _rigidBody= GetComponent<Rigidbody2D>(); // getting the rigid body of the player and will store it in the variable at line 7
@@ -38,36 +121,50 @@ public class PlayerMovement : MonoBehaviour
     // creation of the method onMove(it will recognize user input)
     private void OnMove(InputValue inputValue) // inputValue = the actual imputs given by the user
     {
-        Debug.Log(_movementInput.x);
+        
         _movementInput = inputValue.Get<Vector2>(); //represents the direction and magnitude (force) of the input
         
        
-        if (Input.GetKeyUp("a")) 
+        if (Input.GetKeyUp("a")||Input.GetKey("a")&&Input.GetKeyUp("space")) 
         {
+            isFacing=Facing.Left;
+            
             animator.SetBool("facingLeft",true);
         }
-        if (Input.GetKeyUp("d"))
+        if (Input.GetKeyUp("d")||Input.GetKey("d")&&Input.GetKeyUp("space"))
         {
+            isFacing=Facing.Right;
             animator.SetBool("facingRight",true);
         }
-        if (Input.GetKeyUp("w"))
+        if (Input.GetKeyUp("w")||Input.GetKey("w")&&Input.GetKeyUp("space"))
         {
+            isFacing=Facing.Up;
             animator.SetBool("facingUp",true);
         }
+        if (Input.GetKeyUp("s")||Input.GetKey("s")&&Input.GetKeyUp("space"))
+        {
+             isFacing=Facing.Down;
+        }
+        
         if (Input.anyKey)
         {
+           
             animator.SetBool("facingUp",false);
             animator.SetBool("facingRight",false);
             animator.SetBool("facingLeft",false);
+            
         } 
+        
     }
     
     private async void OnPunch(InputValue inputValue)
     {
         
-
         if (inputValue.isPressed&&Input.GetKey("w")) 
         {
+            isFacing=Facing.Up;
+       
+            punching=true;
             animator.SetBool("punchUp", true);
             speed=0;
             await Task.WhenAll (punchUp());
@@ -75,6 +172,9 @@ public class PlayerMovement : MonoBehaviour
         }
         if (inputValue.isPressed&&Input.GetKey("s"))
         {
+            isFacing=Facing.Down;
+            
+            punching=true;
             animator.SetBool("punchDown", true);
             speed=0;
             await Task.WhenAll (punchDown());
@@ -82,6 +182,10 @@ public class PlayerMovement : MonoBehaviour
         }
         if (inputValue.isPressed&&Input.GetKey("d"))
         {
+            isFacing=Facing.Right;
+            
+            punching=true;
+            
             animator.SetBool("punchRight", true);
             speed=0;
             await Task.WhenAll (punchRight());
@@ -89,6 +193,9 @@ public class PlayerMovement : MonoBehaviour
         }
         if (inputValue.isPressed&&Input.GetKey("a"))
         {
+            isFacing=Facing.Left;
+            punching=true;
+           
             animator.SetBool("punchLeft", true);
             speed=0;
             await Task.WhenAll (punchLeft());
@@ -96,6 +203,7 @@ public class PlayerMovement : MonoBehaviour
         }
         if (inputValue.isPressed)
         {
+            punching=true;
             animator.SetBool("punch",true);
             animator.SetBool("punchUp",false);
             animator.SetBool("punchDown",false);
@@ -104,36 +212,48 @@ public class PlayerMovement : MonoBehaviour
             speed=0;
             await Task.WhenAll (punch());
         }
+        
     }
+
+    
     public async Task punch()
     {
         await Task.Delay(500);
         speed=4;
-        animator.SetBool("punch", false);   
+        animator.SetBool("punch", false);  
+        
     }
 
     public async Task punchUp()
     {
+        
         await Task.Delay(500);
         speed=4;
         animator.SetBool("punchUp", false);   
+        
     }
     public async Task punchDown()
     {
+        
         await Task.Delay(500);
         speed=4;
         animator.SetBool("punchDown", false);   
+        
     }
     public async Task punchRight()
     {
+        
         await Task.Delay(500);
         speed=4;
         animator.SetBool("punchRight", false);   
+        
     }
     public async Task punchLeft()
     {
+        
         await Task.Delay(500);
         speed=4;
         animator.SetBool("punchLeft", false);   
+        
     }
 }
